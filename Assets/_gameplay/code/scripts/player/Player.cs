a351e11cfcd6f270331d5bd800c15e92f5434152
UltraAlucha <87477405+UltraAlucha@users.noreply.github.com>
2025-01-25 01:48:30 +0400

    Changed player prefab

--- Assets/_gameplay/code/scripts/player/Player.cs
+++ Assets/_gameplay/code/scripts/player/Player.cs
@@ -1,9 +1,19 @@
-public class Player : Singleton<Player>
+public class Player : Singleton<Player>, IMainTarget
 {
     // Start is called before the first frame update
+    private UnitHealth _unitHealth;
     public static Player Instance => _instance;
     public override void Awake()
     {
         base.Awake();
+
+        _unitHealth = GetComponent<UnitHealth>();
+
+        TargetManager.Instance.RegisterToManager(this);
+
+        _unitHealth.OnDeath += () =>
+        {
+            TargetManager.Instance.UnregisterToManager(this);
+        };
     }
 }
