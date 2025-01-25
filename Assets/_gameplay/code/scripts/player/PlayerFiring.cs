99883270fb43e9e1b13d7415ee5cf48df37b8032
UltraAlucha <87477405+UltraAlucha@users.noreply.github.com>
2025-01-26 02:00:17 +0400

    Recoil

--- Assets/_gameplay/code/scripts/player/PlayerFiring.cs
+++ Assets/_gameplay/code/scripts/player/PlayerFiring.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net.Http.Headers;
+using DG.Tweening;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -6,18 +8,29 @@ using Random = UnityEngine.Random;
 public class GunSetup
 {
     public GameObject GunObject;
-    public Transform ShootPoint;
     public Projectile ProjectilePrefab;
     public int Damage;
     public int BulletAmount = 1;
     public int SpreadAngle = 10;
 }
 
+[Serializable]
+public class RecoilData
+{
+    public float RecoilPower = 1;
+    public float RecoilDuration = 0.3f;
+}
+
 public class PlayerFiring : MonoBehaviour
 {
     [SerializeField] private float _rayDistance = 100f;
     [SerializeField] private int activeGunIndex = 0;
+    [SerializeField] private Transform _weaponParent;
+    [SerializeField] private Transform _spawnPoint;
     [SerializeField] private GunSetup[] _gunSteps;
+    [SerializeField] private RecoilData _recoilData;
+
+    private Tweener _recoilTween;
 
     private GunSetup ActiveGun => _gunSteps[activeGunIndex];
     bool isFiring = false;
@@ -40,56 +53,66 @@ public class PlayerFiring : MonoBehaviour
 
     void Fire()
     {
-        if (Input.GetMouseButtonUp(0)) // Check for left mouse button press
+        if (Input.GetMouseButtonUp(0))
         {
-            // Calculate the screen center point
-            Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
 
-            // Create a ray from the camera through the screen center
+            Vector3 screenCenter = new(Screen.width / 2f, Screen.height / 2f, 0f);
+
+
             Ray ray = Camera.main.ScreenPointToRay(screenCenter);
 
-            // Perform the raycast
+
             if (Physics.Raycast(ray, out RaycastHit hit, _rayDistance))
             {
-                // If we hit an object, get the world position of the hit
                 Vector3 worldPosition = hit.point;
                 Debug.Log($"Hit at: {worldPosition}");
 
                 ShootProjectile(worldPosition, ActiveGun);
-                // Shoot projectile towards the hit point
             }
             else
             {
-                // If no object is hit, calculate a point in the direction of the ray
                 Vector3 fallbackPosition = ray.origin + ray.direction * _rayDistance;
                 Debug.Log($"No hit detected. Shooting towards: {fallbackPosition}");
                 ShootProjectile(fallbackPosition, ActiveGun);
-                // Shoot projectile towards this fallback position
             }
         }
     }
 
     private void ShootProjectile(Vector3 targetPosition, GunSetup gunData)
     {
+        Recoil();
+
         for (int i = 0; i < gunData.BulletAmount; i++)
         {
-            var projectile = MonoBehaviour.Instantiate(gunData.ProjectilePrefab, gunData.ShootPoint.position, Quaternion.identity, null);
+            var projectile = MonoBehaviour.Instantiate(gunData.ProjectilePrefab, _spawnPoint.position, Quaternion.identity, null);
             var shiftedPositions = GetSpreadDirection(targetPosition, gunData.SpreadAngle);
-            projectile.SetTarget(shiftedPositions, gunData.Damage);
+            if (i == 0)
+            {
+                projectile.SetTarget(targetPosition, gunData.Damage);
+            }
+            else
+            {
+                projectile.SetTarget(shiftedPositions, gunData.Damage);
+            }
+
         }
     }
 
     Vector3 GetSpreadDirection(Vector3 forward, int maxSpreadAngle)
     {
-        // Generate random angles for the spread
         float randomYaw = Random.Range(-maxSpreadAngle, maxSpreadAngle);
         float randomPitch = Random.Range(-maxSpreadAngle, maxSpreadAngle);
 
-        // Create a rotation based on the random angles
         Quaternion spreadRotation = Quaternion.Euler(randomPitch, randomYaw, 0);
 
-        // Apply the rotation to the forward direction
         return spreadRotation * forward;
     }
 
+    private void Recoil()
+    {
+        _recoilTween = _weaponParent.DOLocalMoveZ(-_recoilData.RecoilPower, _recoilData.RecoilDuration).OnComplete(() =>
+        {
+            _recoilTween = _weaponParent.DOLocalMoveZ(0, _recoilData.RecoilDuration);
+        });
+    }
 }
