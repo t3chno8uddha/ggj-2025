be365c32a8fa969c9d5cc26aed62a995df32c948
UltraAlucha <87477405+UltraAlucha@users.noreply.github.com>
2025-01-26 16:15:04 +0400

    Defend Text

---
+++ Assets/_gameplay/code/LookAtCameraNEW.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class LookAtCameraNEW : MonoBehaviour
+{
+    private Transform _cameraTransform;
+    private Transform CameraTransform
+    {
+        get
+        {
+            if (_cameraTransform == null)
+            {
+                _cameraTransform = Camera.main.transform;
+            }
+
+            return _cameraTransform;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(CameraTransform.position);
+    }
+}
+
